90c6dacdfef1d18da91a7c70654374435afaeffd
Renato Lopes <renatolsxc@gmail.com>
2021-04-09 15:10:59 -0300

    correcao_staticfiles

--- django1/settings.py
+++ django1/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
 import os
 from pathlib import Path
 
@@ -122,9 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT = ''
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join('static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')       # usado na produção
+STATIC_URL = '/static/'     # usado no desenvolvimento
+
+#STATICFILES_DIRS = (os.path.join('static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
